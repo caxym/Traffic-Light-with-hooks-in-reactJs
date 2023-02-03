abcd766544fa340a1b92bf602b8163254e2c2ada
raul araque <luis.raul.araque2@gmail.com>
2023-02-03 16:51:52 -0500

    Traffic light with hooks in ReactJs

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from "react";
 
 function App() {
+  const [color, setColor] = useState("");
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='contain-fluid d-flex mt-3'>
+      <div className='semaforo'>
+        <div
+          onClick={() => setColor("red")}
+          className={
+            "light red" + (color === "red" ? " glow" : " offred")
+          }></div>
+        <div
+          onClick={() => setColor("yellow")}
+          className={
+            "light yellow" + (color === "yellow" ? " glow" : " offyellow")
+          }></div>
+        <div
+          onClick={() => setColor("green")}
+          className={
+            "light green" + (color === "green" ? " glow" : " offgreen")
+          }></div>
+      </div>
     </div>
   );
 }
